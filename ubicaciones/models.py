a71e060a5dac4278213a7c2d5aa4441cc0895d5c
Samuel Charles <samyrd309@gmail.com>
2025-04-30 21:34:22 -0400

    Estructura de modelos para app ubicaciones

--- ubicaciones/models.py
+++ ubicaciones/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from usuarios.models import Conductor
 
 # Create your models here.
+
+class UltimaUbicacionAutobus(models.Model):
+    autobus = models.OneToOneField(Conductor, on_delete=models.CASCADE, primary_key=True, related_name='ultima_ubicacion')
+    latitud = models.DecimalField(max_digits=10, decimal_places=8, verbose_name='Latitud')
+    longitud = models.DecimalField(max_digits=11, decimal_places=8, verbose_name='Longitud')
+    fecha_hora = models.DateTimeField(auto_now=True, verbose_name='Fecha y Hora')
+    
+    class Meta:
+        verbose_name = 'Última Ubicación de Autobús'
+        verbose_name_plural = 'Últimas Ubicaciones de Autobús'
+        
+    def __str__(self):
+        return f"Ubicación de {self.autobus.nombre} {self.autobus.apellido} - {self.fecha_hora}"
